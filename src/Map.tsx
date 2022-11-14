62ef148427abcc3aba9da5affb1d03ceb1ef0db4
Joep Meindertsma <joep@ontola.io>
2022-11-14 13:03:45 +0100

    Get google maps working #3

--- src/Map.tsx
+++ src/Map.tsx
@@ -9,27 +9,22 @@ import { mapsKey } from "./config";
 import './Map.css';
 
 export const Map = () => {
-  const [state, set] = useState(false);
   return (
     <div className="Map">
-      <button onClick={() => set(!state)}>rerender</button>
-
-      {window.google && <GeoSearch google={window.google}>
-        {({ hits }) => (
-          <div>
-            <Control />
-            {hits.map((hit) => (
-              <Marker key={hit.objectID} hit={hit} />
-            ))}
-          </div>
-        )}
-      </GeoSearch>}
       <GoogleMapsLoader apiKey={mapsKey}>
         {(google) => {
-          console.log('google', google);
-
-          return (null);
-        }}
+          return (
+          <GeoSearch google={google}>
+            {({ hits }) => (
+              <>
+                <Control />
+                {hits.map((hit) => (
+                  <Marker key={hit.objectID} hit={hit} />
+                ))}
+              </>
+            )}
+          </GeoSearch>
+        )}}
       </GoogleMapsLoader>
     </div>
   );
--- src/import.ts
+++ src/import.ts
@@ -1,10 +1,9 @@
-import { filterProps, Gebouw, sortProps } from './schema';
+import { filterProps, Gebouw } from './schema';
 import { MeiliSearch } from 'meilisearch'
 import testData from '../testData.json'
 import { indexName, server } from './config';
 
 function mapLocation(location: any): Gebouw {
-
   // e.g. "52.106468,5.090403"
   let str = location["bwk-num-lat-lon"];
   let arr = str.split(",");
@@ -20,13 +19,16 @@ function mapLocation(location: any): Gebouw {
   };
 }
 
+const filterableProps = filterProps.map((prop) => prop.propKey);
+filterableProps.push("_geo");
+
 export async function importData() {
   const client = new MeiliSearch({ host: server })
   await client.deleteIndex(indexName)
   const index = client.index(indexName);
   index.addDocuments(testData.map(mapLocation))
     .then((res) => console.log('add document', res))
-  index.updateFilterableAttributes(filterProps.map((prop) => prop.propKey))
+  index.updateFilterableAttributes(filterableProps)
     .then((res) => console.log('apply filterables', res))
   index.updateSortableAttributes(["bag-num-volledig"])
     .then((res) => console.log('apply sortables', res))
