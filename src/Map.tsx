8aaa5c0e5b63d485da56a632f51f1e7a51a5d2a2
Joep Meindertsma <joep@ontola.io>
2022-11-24 14:58:52 +0100

    Add navigation controls

--- src/Map.tsx
+++ src/Map.tsx
@@ -13,6 +13,7 @@ import MapGL, {
   GeolocateControl,
   MapRef,
   Marker,
+  NavigationControl,
 } from "react-map-gl";
 import { LngLatBounds } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -122,7 +123,7 @@ export function Map() {
             latitude={item._geoloc.lat}
             anchor="bottom"
             // We need this key to make sure the content re-renders, for some reason color changes don't trigger an update
-            key={`${item.id} ${isCurrent}`}
+            k={`${item.id} ${isCurrent}`}
             color={isCurrent ? "#000000" : "#FF0000"}
             style={{
               zIndex: isCurrent ? 1 : 0,
@@ -145,6 +146,7 @@ export function Map() {
       ref={mapRef}
       attributionControl={false}
     >
+      <NavigationControl position={"bottom-right"} />
       <GeolocateControl position={"bottom-left"} />
       {markers}
     </MapGL>
