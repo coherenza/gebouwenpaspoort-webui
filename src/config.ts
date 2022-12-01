3061103f7ec287d122de81b04e894a9b6176f168
Joep Meindertsma <joep@ontola.io>
2022-12-01 17:03:05 +0100

    remove trailing slash

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 
 /** Where meilisearch is hosted */
-export const server = import.meta.env["VITE_MEILI_SERVER"] || "https://api.pandata.nl/";
+export const server = import.meta.env["VITE_MEILI_SERVER"] || "https://api.pandata.nl";
 /** Name of the Meilisearch Index */
 export const indexName = 'gbp';
 /** Meilisearch key that needs to provide permission to query, or import (if you use browser import button) */
