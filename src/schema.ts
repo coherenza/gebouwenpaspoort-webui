a9d2244e79660c31110d280d9177fc7a28e1b5b0
Joep Meindertsma <joep@ontola.io>
2022-12-01 12:00:02 +0100

    Add Collection-Attribute schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,46 @@
 import { indexName } from "./config";
 
-// The searchableAttributes determines the attribute ranking order (and also which attributes are searchable).
-// It is also possible to use object-type-sort-order to sort instead of using relevance ranking.
+/** Set of properties for one data source. */
+interface Collection {
+  /** e.g. `Stalen Liggers` or `Bekendmakingen` */
+  name: string;
+  /** first part of JSON key, e.g. `bwk` or `bag` */
+  prefix: string;
+  /** List of available Properties */
+  attributes: Attribute[];
+}
+
+/** A single type of Attribute */
+interface Attribute {
+  /** Human readable string, shown in the front-end */
+  name: string;
+  /** JSON key */
+  id: string;
+  /** Type of the attribute */
+  type: "string" | "number" | "date";
+}
+
+/** The current schema, used for rendering the Details page.
+ *  The ordering of the Collections defines how they are shown in the front-end.
+ */
+const displaySchema: Collection[] = [
+  {
+    name: "Bouwtechnische Kenmerken",
+    prefix: "bwk",
+    attributes: [
+      {
+        name: "Bouwjaar",
+        id: "bouwjaar",
+        type: "number",
+      },
+    ],
+  },
+];
+
+/**
+ * The searchableAttributes determines the attribute ranking order (and also which attributes are searchable).
+ * It is also possible to use object-type-sort-order to sort instead of using relevance ranking.
+ **/
 export const searchableAttributes = [
   "id",
   "naam",
@@ -17,7 +56,7 @@ export const searchableAttributes = [
   "bag-opr-naam-neven",
   "bag-num-volledig",
   "bag-num-volledig-neven",
-  
+
   "bag-aob-oppervlakte",
   "bag-pnd-oorspronkelijk-bouwjaar",
   "bag-aob-gebruiksdoel",
@@ -43,15 +82,19 @@ export const searchableAttributes = [
 ];
 
 export interface SortProp {
-  sortBy: string; /** {index}:{propKey}:{asc/desc} */
+  sortBy: string /** {index}:{propKey}:{asc/desc} */;
   attribute: string;
   label: string;
 }
 
 export const sortProps: SortProp[] = [
-  { sortBy: `${indexName}:object-type-sort-order:asc`, attribute: 'object-type-sort-order',  label: "Object type" },
-  { sortBy: `${indexName}`, attribute: '', label: "Relevantie" },
-  { sortBy: `${indexName}:bag-num-volledig:asc`, attribute: 'bag-num-volledig', label: "Adres" },
+  // { sortBy: `${indexName}:object-type-sort-order:asc`, attribute: 'object-type-sort-order',  label: "Object type" },
+  { sortBy: `${indexName}`, attribute: "", label: "Relevantie" },
+  {
+    sortBy: `${indexName}:bag-num-volledig:asc`,
+    attribute: "bag-num-volledig",
+    label: "Adres",
+  },
 ];
 
 export interface FilterProp {
@@ -64,76 +107,83 @@ export const filterProps: FilterProp[] = [
   { propKey: "bag-object-type", label: "Objecttype", type: "single" },
   { propKey: "bag-aob-gebruiksdoel", label: "Gebruiksdoel", type: "multi" },
   { propKey: "bag-aob-oppervlakte", label: "Oppervlakte (m2)", type: "range" },
-  { propKey: "bag-pnd-oorspronkelijk-bouwjaar", label: "Bouwjaar", type: "range" },
+  {
+    propKey: "bag-pnd-oorspronkelijk-bouwjaar",
+    label: "Bouwjaar",
+    type: "range",
+  },
   { propKey: "epl.pand_energieklasse", label: "Energieklasse", type: "single" },
   { propKey: "bag-pnd-status", label: "Status", type: "multi" },
 ];
 
 export interface GBPObject {
-    "id" : string;
-    "naam" : string;
-    "gbp-collection" : string;
-    "bag-aob-id" : string;
-    "bag-object-type" : string;
-    "object-type-sort-order" : number;
-    "pdok-locatie-id" : string[];
-    "bag-aob-gebruiksdoel" : string[];
-    "bag-aob-oppervlakte" : number;
-    "bag-aob-status" : string;
-    "bag-aob-documentdatum" : Date;
-    "bag-aob-documentnummer" : string;
-    "bag-aob-voorkomen" : number;
-    "bag-num-id" : string;
-    "bag-num-volledig" : string;
-    "bag-num-postcode" : string;
-    "bag-num-huisnummer" : string;
-    "bag-num-huisletter" : string;
-    "bag-num-huisnummertoevoeging" : string;
-    "bag-num-huisnummer-letter-aanduiding" : string;
-    "bag-num-status" : string;
-    "bag-num-documentdatum" : Date;
-    "bag-num-documentnummer" : string;
-    "bag-num-voorkomen" : number;
-    "bag-opr-id" : string;
-    "bag-opr-naam" : string;
-    "bag-opr-type" : string;
-    "bag-opr-status" : string;
-    "bag-opr-documentdatum" : Date;
-    "bag-opr-documentnummer" : string;
-    "bag-opr-voorkomen" : number;
-    "bag-wpl-id" : string;
-    "bag-wpl-naam" : string;
-    "bag-wpl-status" : string;
-    "bag-wpl-documentdatum" : Date;
-    "bag-wpl-documentnummer" : string;
-    "bag-wpl-voorkomen" : number;
-    "bag-pnd-id" : string[];
-    "bag-num-id-neven" : string[];
-    "bag-opr-id-neven" : string[];
-    "bag-wpl-id-neven" : string[];
-    "bag-num-volledig-neven" : string[];
-    "bag-num-postcode-neven" : string[];
-    "bag-opr-naam-neven" : string[];
-    "bag-pnd-oorspronkelijk-bouwjaar" : number[];
-    "bag-pnd-status" : string[];
-    "bwk-num-nadergebruiksdoel" : string[];
-    "bwk-num-status" : string[];
-    "bwk-num-adrestype" : string[];
-    "_geo" : { "lat" : number; "lng" : number };
-    "bag-aob-geo-EPSG28992" : { "x" : number; "y" : number; "z" : number };
-    "bwk-num-geo-EPSG28992" : { "x" : number; "y" : number };
-    "geo_bbox" : { "lat" : number; "lng" : number }[];
-    "geo_polygon" : number[];
-    "bwk-num-wijkid" : string;
-    "bwk-num-wijkcode" : string;
-    "bwk-num-wijknaam" : string;
-    "bwk-num-subwijkid" : string;
-    "bwk-num-subwijkcode" : string;
-    "bwk-num-subwijknaam" : string;
-    "bwk-num-buurtid" : string;
-    "bwk-num-buurtcode" : string;
-    "bwk-num-buurtnaam" : string;
-    "bwk-num-subbuurtid" : string;
-    "bwk-num-subbuurtcode" : string;
-    "bwk-num-subbuurtnaam" : string;
+  id: string;
+  naam: string;
+  "gbp-collection": string;
+  "bag-aob-id": string;
+  "bag-object-type": string;
+  "object-type-sort-order": number;
+  "pdok-locatie-id": string[];
+  "bag-aob-gebruiksdoel": string[];
+  "bag-aob-oppervlakte": number;
+  "bag-aob-status": string;
+  "bag-aob-documentdatum": Date;
+  "bag-aob-documentnummer": string;
+  "bag-aob-voorkomen": number;
+  "bag-num-id": string;
+  "bag-num-volledig": string;
+  "bag-num-postcode": string;
+  "bag-num-huisnummer": string;
+  "bag-num-huisletter": string;
+  "bag-num-huisnummertoevoeging": string;
+  "bag-num-huisnummer-letter-aanduiding": string;
+  "bag-num-status": string;
+  "bag-num-documentdatum": Date;
+  "bag-num-documentnummer": string;
+  "bag-num-voorkomen": number;
+  "bag-opr-id": string;
+  "bag-opr-naam": string;
+  "bag-opr-type": string;
+  "bag-opr-status": string;
+  "bag-opr-documentdatum": Date;
+  "bag-opr-documentnummer": string;
+  "bag-opr-voorkomen": number;
+  "bag-wpl-id": string;
+  "bag-wpl-naam": string;
+  "bag-wpl-status": string;
+  "bag-wpl-documentdatum": Date;
+  "bag-wpl-documentnummer": string;
+  "bag-wpl-voorkomen": number;
+  "bag-pnd-id": string[];
+  "bag-num-id-neven": string[];
+  "bag-opr-id-neven": string[];
+  "bag-wpl-id-neven": string[];
+  "bag-num-volledig-neven": string[];
+  "bag-num-postcode-neven": string[];
+  "bag-opr-naam-neven": string[];
+  "bag-pnd-oorspronkelijk-bouwjaar": number[];
+  "bag-pnd-status": string[];
+  "bwk-num-nadergebruiksdoel": string[];
+  "bwk-num-status": string[];
+  "bwk-num-adrestype": string[];
+  _geo: { lat: number; lng: number };
+  "bag-aob-geo-EPSG28992": { x: number; y: number; z: number };
+  "bwk-num-geo-EPSG28992": { x: number; y: number };
+  geo_bbox: { lat: number; lng: number }[];
+  geo_polygon: number[];
+  "bwk-num-wijkid": string;
+  "bwk-num-wijkcode": string;
+  "bwk-num-wijknaam": string;
+  "bwk-num-subwijkid": string;
+  "bwk-num-subwijkcode": string;
+  "bwk-num-subwijknaam": string;
+  "bwk-num-buurtid": string;
+  "bwk-num-buurtcode": string;
+  "bwk-num-buurtnaam": string;
+  "bwk-num-subbuurtid": string;
+  "bwk-num-subbuurtcode": string;
+  "bwk-num-subbuurtnaam": string;
+  stl: undefined | Record<string, any>[];
+  squitxo: undefined | Record<string, any>[];
+  bekendmakingen: undefined | Record<string, any>[];
 }
