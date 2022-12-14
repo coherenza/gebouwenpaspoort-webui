b9ae801cb889c95aaa6522b4e75503719c841cab
Nico Verwer <nverwer@gmail.com>
2022-12-14 20:24:51 +0100

    contrary to the documentation, default operator is 'and' instead of 'or'
    Like most modern, trendy javascript frameworks and sDKs, the documentation [https://www.algolia.com/doc/api-reference/widgets/refinement-list/react-hooks/]  is wrong. But if you are under 50, it does not matter because you don't read (or write) documentation.

--- src/Filter.tsx
+++ src/Filter.tsx
@@ -23,10 +23,10 @@ export const Filter = (filter: Attribute) => {
   return (
     <div className="Filter">
       <h4>{filter.name}</h4>
-      {filterType === "select" && (<RefinementList attribute={filter.id} />)}
-      {/* {filterType === "range" && (<RangeSlider attribute={filter.id} />)} */}
-      {filterType === "range" && (<RangeInput attribute={filter.id} />)}
-      {filterType === "intervals" && (<RefinementList attribute={filter.id} sortBy={sortIntervals} />)}
+      {filterType === "select" && (<RefinementList attribute={filter.id} operator="or"/>)}
+      {/* {filterType === "range" && (<RangeSlider attribute={filter.id}/>)} */}
+      {filterType === "range" && (<RangeInput attribute={filter.id}/>)}
+      {filterType === "intervals" && (<RefinementList attribute={filter.id} sortBy={sortIntervals} operator="or"/>)}
     </div>
   );
 };
