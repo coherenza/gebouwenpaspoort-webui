831106c8bb8dce43111c10a1581a828c582a3e48
Joep Meindertsma <joep@ontola.io>
2022-12-15 13:41:58 +0100

    Remove operator from refinementlist

--- src/Filter.tsx
+++ src/Filter.tsx
@@ -1,19 +1,19 @@
 import { Attribute } from "./schema";
 import React from "react";
-import {
-  RangeInput,
-  RefinementListProps,
-} from "react-instantsearch-hooks-web";
+import { RangeInput, RefinementListProps } from "react-instantsearch-hooks-web";
 import "./Filter.css";
 import { RefinementList } from "./RefinementList";
 
 // Sort strings that we use to indicate numeric intervals.
-const intervalSortValue = function(x: string) {
-  const value = x.replace(/^\D+/, '0.').replace(/\D+$/, '.0').replace(/\s*-\s*/, '.');
+const intervalSortValue = function (x: string) {
+  const value = x
+    .replace(/^\D+/, "0.")
+    .replace(/\D+$/, ".0")
+    .replace(/\s*-\s*/, ".");
   return Number.parseFloat(value);
 };
 
-const sortIntervals: RefinementListProps['sortBy'] = (a, b) => {
+const sortIntervals: RefinementListProps["sortBy"] = (a, b) => {
   return intervalSortValue(a.name) < intervalSortValue(b.name) ? -1 : 1;
 };
 
@@ -22,9 +22,17 @@ export const Filter = (filter: Attribute) => {
   return (
     <div className="Filter">
       <h4>{filter.name}</h4>
-      {filterType === "select" && (<RefinementList attribute={filter.id} limit={10} />)}
-      {filterType === "range" && (<RangeInput attribute={filter.id}/>)}
-      {filterType === "intervals" && (<RefinementList attribute={filter.id} sortBy={sortIntervals} operator="or" limit={16}/>)}
+      {filterType === "select" && (
+        <RefinementList attribute={filter.id} limit={10} />
+      )}
+      {filterType === "range" && <RangeInput attribute={filter.id} />}
+      {filterType === "intervals" && (
+        <RefinementList
+          attribute={filter.id}
+          sortBy={sortIntervals}
+          limit={16}
+        />
+      )}
     </div>
   );
 };
