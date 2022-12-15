153b901cb23ab30dea91143076e0198410f3d76a
Joep Meindertsma <joep@ontola.io>
2022-12-15 10:18:01 +0100

    Custom RefinementList

--- src/Filter.tsx
+++ src/Filter.tsx
@@ -2,18 +2,17 @@ import { Attribute } from "./schema";
 import React from "react";
 import {
   RangeInput,
-  RefinementList,
   RefinementListProps,
 } from "react-instantsearch-hooks-web";
 import "./Filter.css";
-import { RangeSlider } from "./RangeSlider";
+import { RefinementList } from "./RefinementList";
 
 // Sort strings that we use to indicate numeric intervals.
 const intervalSortValue = function(x: string) {
   const value = x.replace(/^\D+/, '0.').replace(/\D+$/, '.0').replace(/\s*-\s*/, '.');
   return Number.parseFloat(value);
 };
-  
+
 const sortIntervals: RefinementListProps['sortBy'] = (a, b) => {
   return intervalSortValue(a.name) < intervalSortValue(b.name) ? -1 : 1;
 };
@@ -23,8 +22,7 @@ export const Filter = (filter: Attribute) => {
   return (
     <div className="Filter">
       <h4>{filter.name}</h4>
-      {filterType === "select" && (<RefinementList attribute={filter.id} operator="or" limit={16}/>)}
-      {/* {filterType === "range" && (<RangeSlider attribute={filter.id}/>)} */}
+      {filterType === "select" && (<RefinementList attribute={filter.id} limit={10} />)}
       {filterType === "range" && (<RangeInput attribute={filter.id}/>)}
       {filterType === "intervals" && (<RefinementList attribute={filter.id} sortBy={sortIntervals} operator="or" limit={16}/>)}
     </div>
---
+++ src/RefinementList.tsx
@@ -0,0 +1,71 @@
+import { RefinementListItem } from "instantsearch.js/es/connectors/refinement-list/connectRefinementList";
+import { useEffect, useState } from "react";
+import { useRefinementList } from "react-instantsearch-hooks-web";
+import "./RefinementList.css";
+
+interface RefinementListProps {
+  attribute: string;
+  sortBy?: any;
+  limit?: number;
+}
+
+export function RefinementList({ attribute, sortBy, limit = 10 }: RefinementListProps) {
+  const { items, refine } =
+    useRefinementList({
+      attribute,
+      operator: "or",
+      sortBy,
+      // We use a large limit, because we do our own 'show more' button.
+      limit: 30,
+  });
+
+  const [savedItems, setSavedItems] = useState<RefinementListItem[]>([]);
+  const [showMore, setShowMore] = useState(false);
+
+  useEffect(() => {
+    if (items.length > savedItems.length) {
+      setSavedItems(items);
+    } else {
+      // Do not update savedItems that are missing in the new `items`.
+      // This is to make sure the list of checkboxes shows all options - even if some options are no longer in the items list.
+      const newSavedItems = savedItems.map((savedItem) => {
+        const newItem = items.find((item) => item.label === savedItem.label);
+        // However, we do need to remove items that are unchecked.
+        // If the user unchecks an item, we don't want it to appear checked in the next render
+        // because that would confuse the user.
+        if (!newItem) {
+          savedItem.isRefined = false;
+          return savedItem;
+        } else {
+          return newItem;
+        }
+      });
+      setSavedItems(newSavedItems);
+    }
+  }, [items]);
+
+  const limitedSavedItems = showMore ? savedItems : savedItems.slice(0, limit);
+
+  return (
+    <div className="RefinementList">
+      <ul>
+        {limitedSavedItems.map((item) => (
+          <li key={item.label}>
+            <label>
+              <input
+                type="checkbox"
+                checked={item.isRefined}
+                onChange={(_event) => refine(item.value)}
+              />
+              <span>{item.label}</span>
+              <span className="RefinementList__count">{item.count}</span>
+            </label>
+          </li>
+        ))}
+      </ul>
+      {savedItems.length > limit && <button onClick={() => setShowMore(!showMore)}>
+        {showMore ? "minder" : "meer"}
+      </button>}
+    </div>
+  );
+}
