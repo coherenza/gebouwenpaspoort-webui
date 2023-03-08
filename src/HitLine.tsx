edf5d1ab326218bff096bc7087d83b4357caa613
Joep Meindertsma <joep@ontola.io>
2023-03-08 09:11:12 +0100

    Stripe through non-existing buildings

--- src/HitLine.tsx
+++ src/HitLine.tsx
@@ -79,7 +79,11 @@ export const HitLine = ({ hit }: HitProps) => {
   return (
     <div className={active ? "Hit Hit--active" : "Hit"} onClick={handleClick}>
       {/* Click on area-filter -> set filter on pdok-locatie-id == hit.id */}
-      <div className="hit-naam">
+      <div
+        className={`hit-naam ${
+          status && shouldShowStatus(status) ? "hit-naam__deleted" : ""
+        }`}
+      >
         {isAob ? "" : "🔍 "}
         {isAob ? adres : hit["naam"]}
       </div>
