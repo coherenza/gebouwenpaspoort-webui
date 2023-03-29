fd0ef5611a245b429de393cf33bbb2efff01ba18
Joep Meindertsma <joep@ontola.io>
2023-03-29 10:44:13 +0200

    Update api URL

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-
 /** Where meilisearch is hosted */
-export const server = import.meta.env["VITE_MEILI_SERVER"] || "https://api.pandata.nl";
+export const server =
+  import.meta.env["VITE_MEILI_SERVER"] || "https://api-staging.pandata.nl";
 /** Name of the Meilisearch Index */
-export const indexName = 'gbp';
+export const indexName = "gbp";
 /** Meilisearch key that needs to provide permission to query, or import (if you use browser import button) */
 export const meiliKey = import.meta.env["VITE_MEILI_API_KEY"];
 /** 'production' or 'development' */
