15c15729667eb5166ec609dc4cf874613aafcf9d
Jan Meijer <jan.meijer@coherenza.com>
2023-07-06 16:32:19 +0200

    Coherenza key for Mapbox

--- src/config.ts
+++ src/config.ts
@@ -8,4 +8,7 @@ export const indexName = "gbp";
 /** Meilisearch key that needs to provide permission to query, or import (if you use browser import button) */
 export const meiliKey = import.meta.env["VITE_MEILI_API_KEY"];
 /** 'production' or 'development' */
+/* Key Joep
 export const mapboxToken = "pk.eyJ1Ijoiam9lcGlvIiwiYSI6ImNqbTIzanZ1bjBkanQza211anFxbWNiM3IifQ.2iBrlCLHaXU79_tY9SVpXA";
+*/
+export const mapboxToken = "pk.eyJ1IjoiY29oZXJlbnphIiwiYSI6ImNsanI4MXI0cTBmaWMzc3FuNTQwZTMwamUifQ.tolQPMM1XDvhX0DW7k3neg";
