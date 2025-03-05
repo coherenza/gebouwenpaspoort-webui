71a94acfb6e30de1335164cfea2a24ced89511e7
Nico Verwer <nverwer@rakensi.com>
2025-03-05 14:17:48 +0100

    new icons added

--- src/Icons.ts
+++ src/Icons.ts
@@ -1,9 +1,34 @@
-import MarkerPng from "./assets/map-marker.png";
+import Marker from "./assets/map-marker.png";
+import City from "./assets/city.png";
+import District from "./assets/district.png";
+import SubDistrict from "./assets/sub-district.png";
+import Neighborhood from "./assets/neighborhood.png";
+import SubNeighborhood from "./assets/sub-neighborhood.png";
 import Houses from "./assets/houses.png";
 const CustomIcons = [
   {
-    src: MarkerPng,
+    src: Marker,
     name: "my-marker",
+  },
+  {
+    src: City,
+    name: "city",
+  },
+  {
+    src: District,
+    name: "district"
+  },
+  {
+    src: SubDistrict,
+    name: "sub-district",
+  },
+  {
+    src: Neighborhood,
+    name: "neighborhood",
+  },
+  {
+    src: SubNeighborhood,
+    name: "sub-neighborhood",
   },
   {
     src: Houses,
